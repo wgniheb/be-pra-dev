5d6dc409e701b08d92e79a77e431304cbad57671
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2024-01-14 21:08:29 +0700

    Update phone_number column type in users table

--- database/migrations/2023_12_31_144917_add_phone_number_column_to_users_table.php
+++ database/migrations/2023_12_31_144917_add_phone_number_column_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->bigInteger('phone_number')->after('password')->nullable();
+            $table->string('phone_number')->after('password')->nullable();
         });
     }
 
