99e1fffbdcebc8a28e09863ce97677f11eb37a18
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2024-02-09 21:42:27 +0700

    Update description column type to text in migrations

--- database/migrations/2023_11_21_082750_create_issue_matrixs_table.php
+++ database/migrations/2023_11_21_082750_create_issue_matrixs_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('issue_matrices', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
+            $table->text('description');
             $table->timestamps();
             $table->softDeletes();
         });
